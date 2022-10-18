a2f7a286032c489363a4396994943bbfeccfd4df
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-10-17 23:53:47 -0300

    feat: calorias abaixo de 500

---
+++ challenges/desafio9.js
@@ -0,0 +1,15 @@
+db.produtos.find(
+  { 
+    valoresNutricionais: 
+    { 
+      $elemMatch: 
+      { tipo: "calorias",
+        quantidade: { $lt: 500 },
+      }, 
+    },
+  },
+  {
+    _id: 0,
+    nome: 1,
+  },
+);
